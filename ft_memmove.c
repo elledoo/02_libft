4a62103ead17bdd92c36defd06b510dae01ba165
Ekaterina SUSLOVA <esuslova@e3r8p7.42.fr>
2019-04-14 16:00:25 +0200

    Corrected memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: esuslova <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 11:51:12 by esuslova          #+#    #+#             */
-/*   Updated: 2019/04/07 13:16:26 by esuslova         ###   ########.fr       */
+/*   Updated: 2019/04/14 15:59:32 by esuslova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,22 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char temp[len];
+	unsigned char		*pd;
+	const unsigned char	*ps;
 
-	ft_memcpy(temp, src, len);
-	ft_memcpy(dst, temp, len);
+	pd = (unsigned char *)dst;
+	ps = (const unsigned char *)src;
+	if (src < dst)
+	{
+		pd = pd + len - 1;
+		ps = ps + len - 1;
+		while (len--)
+			(*pd-- = *ps--);
+	}
+	else
+	{
+		while (len--)
+			*pd++ = *ps++;
+	}
 	return (dst);
 }
